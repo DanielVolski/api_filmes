d37d3a1368e030c2842e7878101255d54823b280
yorranan <yorranan.costa@outlook.com>
2023-08-23 18:02:51 -0300

    class User created

--- src/main/java/api_filmes/domain/entities/User.java
+++ src/main/java/api_filmes/domain/entities/User.java
@@ -1,5 +1,46 @@
 package api_filmes.domain.entities;
 
+import java.util.Date;
+import java.util.List;
+
 public class User {
+    private String id;
+    private String name;
+    private String password;
+    private Date activationDate;
+    private Date inactivationDate;
+    private List<Movie> movie;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Date getActivationDate() {
+        return activationDate;
+    }
+    public void setActivationDate(Date activationDate) {
+        this.activationDate = activationDate;
+    }
+    public Date getInactivationDate() {
+        return inactivationDate;
+    }
+    public void setInactivationDate(Date inactivationDate) {
+        this.inactivationDate = inactivationDate;
+    }
+
     
 }
