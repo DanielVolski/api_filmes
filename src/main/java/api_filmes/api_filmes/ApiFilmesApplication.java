2c8a067428c3e973a0c9eabb93b5c38d6688ae5e
Daniel Volski <danfvdaum3@hotmail.com>
2023-08-23 17:18:08 -0300

    First commit

---
+++ src/main/java/api_filmes/api_filmes/ApiFilmesApplication.java
@@ -0,0 +1,13 @@
+package api_filmes.api_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api_filmes/api_filmes/ApiFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package api_filmes.api_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
