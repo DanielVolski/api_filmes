711d0434c11ab9502122c6febfa45dca028953e4
Daniel Volski <danfvdaum3@hotmail.com>
2023-09-01 17:10:59 -0300

    Creating MovieRequstDTO and MovieResponseDTO

---
+++ src/main/java/api_filmes/domain/dto/movie/MovieResponseDTO.java
@@ -0,0 +1,37 @@
+package api_filmes.domain.dto.movie;
+
+import java.util.Date;
+
+public class MovieResponseDTO {
+    private Long id;
+    private String title;
+    private String director;
+    private Date releaseDate;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    
+}
