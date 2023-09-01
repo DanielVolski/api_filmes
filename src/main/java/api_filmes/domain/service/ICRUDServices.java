fdb0f59a4ea54731f8ea80620d33e52c40289b63
Daniel Volski <danfvdaum3@hotmail.com>
2023-09-01 16:30:21 -0300

    Creating ICRUDService

---
+++ src/main/java/api_filmes/domain/service/ICRUDServices.java
@@ -0,0 +1,11 @@
+package api_filmes.domain.service;
+
+import java.util.List;
+
+public interface ICRUDServices<Request, Response> {
+    List<Response> getAll();
+    Response getById(Long id);
+    Response create(Request dto);
+    Response update(Long id, Request dto);
+    void delete(Long id);     
+}
