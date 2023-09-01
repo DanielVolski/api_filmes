de878543f0874cd49453c7e0732aca7c2999aaed
Daniel Volski <danfvdaum3@hotmail.com>
2023-09-01 17:22:04 -0300

    Creating MovieService

---
+++ src/main/java/api_filmes/domain/service/MovieService.java
@@ -0,0 +1,49 @@
+package api_filmes.domain.service;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import api_filmes.domain.repository.MovieRepository;
+
+@Service
+public class MovieService implements ICRUDService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public List getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Object getById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    @Override
+    public Object create(Object dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public Object update(Long id, Object dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+    
+}
