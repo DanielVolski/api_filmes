99cc7cba6ba63fdce6df11d59363125d7d0fa184
yorranan <yorranan.costa@outlook.com>
2023-09-05 12:07:44 -0300

    updated User entity

--- src/main/java/api_filmes/domain/entities/User.java
+++ src/main/java/api_filmes/domain/entities/User.java
@@ -1,6 +1,10 @@
 package api_filmes.domain.entities;
 
+import java.util.Collection;
 import java.util.Date;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,7 +15,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idUser") 
@@ -60,5 +64,29 @@ public class User {
     }
     public void setInactivationDate(Date inactivationDate) {
         this.inactivationDate = inactivationDate;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
